35923aff6c8c35ff0a7e33b73e617c536d73af05
Badi-Haki Martin <jojackblack@gmail.com>
2022-03-21 13:48:29 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(username, userEmail){
+    const body = document.querySelector('body');
+    const user={
+        'name': username,
+        'email': userEmail,
+    }
+    return fetch('http://localhost:3000/users',{
+        method: `POST`,
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify(user)
+    }).then(
+        response => response.json()
+    ).then(
+        newObj => {
+            // find the ID of newObj and append the value to the DOM within the body
+            // snag the body
+            // create a new 'p' element to hold the ID in
+            //const p = document.createElement('p');
+            //p.innerText = newObj.id;
+            // append to the body
+            //body.appendChild(p);
+            body.innerHTML=newObj.id;
+        }
+    ).catch(
+        error => {
+            body.innerHTML='Unauthorized Access';
+        }
+    )
+}
